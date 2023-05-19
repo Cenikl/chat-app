1b789372d3bb7b5d1115df19adb5d34ea3e31d59
Cedric <99885260+Cenikl@users.noreply.github.com>
2023-05-19 16:06:13 +0300

    feat/ messages

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -2,7 +2,22 @@ import Cookies from 'universal-cookie';
 import styles from '../../styles/Main.module.css';
 import axios from 'axios';
 import {GetServerSideProps, NextApiRequest, NextApiResponse } from 'next';
+import { useState } from 'react';
 
+type Message = {
+    id: number,
+    content: string,
+    createdAt: string,
+    updatedAt: string,
+    senderId: number,
+    recipientId: null,
+    channelId: number,
+    sender: {
+        id: number,
+        name: string,
+        email: string
+    }
+}
 
 export const getServerSideProps: GetServerSideProps = async (context) =>{
     const {req} = context;
@@ -22,22 +37,48 @@ export const getServerSideProps: GetServerSideProps = async (context) =>{
         }
     })
     const data = await request.json()
-    console.log(data)
     return {
         props:{
-            channels:data
-        }
+            channels:data.channels,
+        },
     }
 }
 
-export default function menu(){
+export default function menu({channels}:any){
+    const [messages,setMessages] = useState<Message[]>([]);
+    const cookies = new Cookies();
+    const focusChat = async (id:any) => {
+        const request = await fetch('http://localhost:8080/messages/channel/'+id,{
+            headers:{
+                'Authorization' : 'Bearer '+cookies.get('jwttoken'),
+                'Content-Type': 'application/json'
+            }
+        })
+        const response = await request.json();
+        setMessages(response.messages)
+        messages?.sort((a,b) => b.id - a.id)
+        return "hello world" 
+    }
     return (
         <>
         <div className={styles.main}>
             <div className={styles.channel}>
+            {channels.map((channel:any) => {
+                return(
+                    <div key={channel.id}>
+                        <button onClick={() => focusChat(channel.id)}>{channel.name}</button>
+                    </div>
+                )
+            })}
             </div>
             <div className={styles.messages}>
-
+            {messages.map((message:any) => {
+                return(
+                    <div key={message.id}>
+                        <p>{message.content}</p>
+                    </div>
+                )
+            })}
             </div>
             <div className={styles.members}>
 
